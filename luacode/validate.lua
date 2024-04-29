aa09c11edf2cac6c938cdd6385cad8c4d3701d24
Barrett Brown <barrett.brown@geappliances.com>
2024-04-29 09:50:31 -0400

    Use loadfile

--- luacode/validate.lua
+++ luacode/validate.lua
@@ -1,10 +1,4 @@
-local handle = io.open(arg[1], "r")
-if not handle then return end
-
-local source = handle:read("*all")
-handle:close()
-
-local status, message = load(source)
+local status, message = loadfile(arg[1])
 if not status then
   print("Parse Error\n" .. message)
   os.exit(1)
