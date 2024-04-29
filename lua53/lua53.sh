0b1be3ee4f34267ef3f0722368f4e284aa912767
Barrett Brown <barrett.brown@geappliances.com>
2024-04-29 09:44:04 -0400

    Update bundled lua for windows

--- lua53/lua53.sh
+++ lua53/lua53.sh
@@ -1,17 +1,22 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
-if [ "$(uname)" = "Darwin" ]; then
-  if [ "$(uname -m)" = "x86_64" ]; then
-    exec $SCRIPTPATH/osx/lua53 $@
+PLATFORM=$(uname)
+ARCH=$(uname -m)
+
+if [ $PLATFORM = "Darwin" ]; then
+  if [ $ARCH = "x86_64" ]; then
+    exec $SCRIPTPATH/macos/lua53 $@
   elif [ "$(uname -m)" = "arm64" ]; then
-    exec $SCRIPTPATH/macos-arm64/lua $@
+    exec $SCRIPTPATH/macos-arm64/lua53 $@
+  fi
+elif [ $PLATFORM = "Linux" ]; then
+  if [ $ARCH = "x86_64" ]; then
+    exec $SCRIPTPATH/linux/lua53 $@
+  elif [ $ARCH = "aarch64" ]; then
+    exec $SCRIPTPATH/linux-arm64/lua53 $@
+  elif [ $ARCH = "armv7l" ]; then
+    exec $SCRIPTPATH/armv7l/lua53 $@
   fi
 else
-  if [ "$(uname -m)" = "x86_64" ]; then
-    exec $SCRIPTPATH/linux/lua53 $@
-  elif [ "$(uname -m)" = "aarch64" ]; then
-    exec $SCRIPTPATH/linux-arm64/lua53 $@
-  elif [ "$(uname -m)" = "armv7l" ]; then
-    exec $SCRIPTPATH/armv7l/lua $@
-  fi
+  exec $SCRIPTPATH/windows/lua53.exe $@
 fi
