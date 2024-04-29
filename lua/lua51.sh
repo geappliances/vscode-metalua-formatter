5ffb01127588e76ba32ecded2795139c8b81e2a8
Barrett Brown <barrett.brown@geappliances.com>
2024-04-29 11:07:37 -0400

    Add lua 5.1 binaries

---
+++ lua/lua51.sh
@@ -0,0 +1,21 @@
+SCRIPTPATH=$(
+  cd $(dirname $0)
+  pwd -P
+)
+
+PLATFORM=$(uname)
+ARCH=$(uname -m)
+
+if [ $PLATFORM = "Darwin" ]; then
+  if [ $ARCH = "x86_64" ]; then
+    exec $SCRIPTPATH/macos/lua51 $@
+  elif [ "$(uname -m)" = "arm64" ]; then
+    exec $SCRIPTPATH/macos-arm64/lua51 $@
+  fi
+else
+  if [ $ARCH = "x86_64" ]; then
+    exec $SCRIPTPATH/linux/lua51 $@
+  elif [ $ARCH = "aarch64" ]; then
+    exec $SCRIPTPATH/linux-arm64/lua51 $@
+  fi
+fi
--- lua/lua53.sh
+++ lua/lua53.sh
@@ -9,7 +9,7 @@ if [ $PLATFORM = "Darwin" ]; then
   elif [ "$(uname -m)" = "arm64" ]; then
     exec $SCRIPTPATH/macos-arm64/lua53 $@
   fi
-elif [ $PLATFORM = "Linux" ]; then
+else
   if [ $ARCH = "x86_64" ]; then
     exec $SCRIPTPATH/linux/lua53 $@
   elif [ $ARCH = "aarch64" ]; then
@@ -17,6 +17,4 @@ elif [ $PLATFORM = "Linux" ]; then
   elif [ $ARCH = "armv7l" ]; then
     exec $SCRIPTPATH/armv7l/lua53 $@
   fi
-else
-  exec $SCRIPTPATH/windows/lua53.exe $@
 fi
